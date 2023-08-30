98767bdef6b13a67469d6e1d19f0188d750d256d
Zero <yasseravalosmontero14@gmail.com>
2023-08-30 17:51:59 -0500

    Inicio del proyecto

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace proyecto_ecommerce_deportivo_net.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
