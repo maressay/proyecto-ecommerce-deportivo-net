75c0a141c16163969d1acbc1cf98f332182aab26
MATIASARAUJ0 <araujovmatias@gmail.com>
2023-11-12 04:37:56 -0500

    design v1

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -81,7 +81,7 @@ namespace proyecto_ecommerce_deportivo_net.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Display(Name = "¿Acuérdate de mí?")]
+            [Display(Name = "Recordar sesión")]
             public bool RememberMe { get; set; }
         }
 
