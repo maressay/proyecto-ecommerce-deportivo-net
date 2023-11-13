5ded3aab6e5688319f2629b6bfd6295ce92fa30b
jesus73514145 <jesus_soria@usmp.pe>
2023-11-13 17:31:19 -0500

    Arreglando el gestionar perfil
    Co-authored-by: KristellFlores <kristell_flores@usmp.pe>

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -8,12 +8,15 @@ namespace proyecto_ecommerce_deportivo_net.Models {
     public class ApplicationUser : IdentityUser {
 
         //[Required] // Esto asegura que el campo no puede ser null
+        [PersonalData]
         public string? Nombres { get; set; }
 
         //[Required] // Esto asegura que el campo no puede ser null
+        [PersonalData]
         public string? Apellidos { get; set; }
 
         //[Required] // Esto asegura que el campo no puede ser null
+        [PersonalData]
         public string? Dni { get; set; }
 
         public string Rol { get; set; } = "Cliente"; // Atributo nuevo para el rol, por defecto es "Cliente"
